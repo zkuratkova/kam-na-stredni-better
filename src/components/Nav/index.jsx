99da68a71e6e204ce698d1687886fdbd79fb40be
Zuzana Kuřátková <zkuratkova@gmail.com>
2021-05-26 16:00:25 +0200

    useState otevřené a zavřené menu

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavItem from '../NavItem';
 import './style.css';
 
 const Nav = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+  const handleClick = () => setMenuOpened(!menuOpened);
   const navigation = [
     { text: 'Domů', href: '/' },
     { text: 'Lekce', href: '/souhrn-lekci' },
@@ -11,8 +13,8 @@ const Nav = () => {
   ];
 
   return (
-    <nav className="nav">
-      <ul className="nav__list nav__list--close">
+    <nav className="nav" onClick={handleClick}>
+      <ul className={menuOpened ? 'nav__list' : 'nav__list nav__list--close'}>
         {navigation.map((item) => (
           <NavItem key={item.text} textContent={item.text} href={item.href} />
         ))}
