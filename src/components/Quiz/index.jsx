84eed0263d8c59adc2f482c22a02d89f3fab6e4d
Zuzana Kuřátková <zkuratkova@gmail.com>
2021-05-29 18:45:19 +0200

    status answerRusult

--- src/components/Quiz/index.jsx
+++ src/components/Quiz/index.jsx
@@ -8,95 +8,53 @@ const Quiz = (props) => {
   idLekce = idLekce - 1;
   const aktualniLekce = quiz[idLekce];
 
+  const [answerResult, setAnswerResult] = useState('wrong'); // right, wrong, not-answered
+
   return (
     <>
-      <form className="quiz">
+      <form
+        className={`quiz ${answerResult === 'right' && 'quiz--right'} ${
+          answerResult === 'wrong' && 'quiz--wrong'
+        } `}
+      >
         <p className="quiz__question">{aktualniLekce.question}</p>
         <div className="quiz__answer">
           <label>
             <input type="radio" name="quiz" id="first" />
-            1. {aktualniLekce.answer1}
+            1.{aktualniLekce.answer1}
           </label>
         </div>
         <div className="quiz__answer">
           <label>
             <input type="radio" name="quiz" id="second" />
-            2. {aktualniLekce.answer2}
+            2.{aktualniLekce.answer2}
           </label>
         </div>
         <div className="quiz__answer">
           <label>
             <input type="radio" name="quiz" id="third" />
-            3. {aktualniLekce.answer3}
+            3.{aktualniLekce.answer3}
           </label>
         </div>
         <div className="quiz__btn">
-          <Btn text={'Odeslat'} btnType={'btn'} />
-        </div>
-      </form>
-
-      <form className="quiz quiz--right">
-        <p className="quiz__question">
-          Na co se buseš ptát svého okolí v souvislosti s hledáním vhodné školy?
-        </p>
-        <div className="quiz__answer">
-          <label>
-            <input type="radio" name="quiz" id="first" />
-            1. Kam bys šel ty?
-          </label>
-        </div>
-        <div className="quiz__answer">
-          <label>
-            <input type="radio" name="quiz" id="second" />
-            2. Jak moc je pravděpodobné, že zdědím hrozně moc peněz a budu
-            rentiér?
-          </label>
-        </div>
-        <div className="quiz__answer">
-          <label>
-            <input type="radio" name="quiz" id="third" />
-            3. Co na mě obdivuješ?
-          </label>
-        </div>
-        <div className="quiz__btn">
-          <p className="quiz__text quiz__text--right">
-            Super! Máš to správně. Chceš jít dál?
-          </p>
-          <Btn text={'Další lekce'} linkTo={'/lekce'} btnType={'right'} />
-        </div>
-      </form>
-
-      <form className="quiz quiz--wrong">
-        <p className="quiz__question">
-          Na co se buseš ptát svého okolí v souvislosti s hledáním vhodné školy?
-        </p>
-        <div className="quiz__answer">
-          <label>
-            <input type="radio" name="quiz" id="first" />
-            1. Kam bys šel ty?
-          </label>
-        </div>
-        <div className="quiz__answer">
-          <label>
-            <input type="radio" name="quiz" id="second" />
-            2. Jak moc je pravděpodobné, že zdědím hrozně moc peněz a budu
-            rentiér?
-          </label>
-        </div>
-        <div className="quiz__answer">
-          <label>
-            <input type="radio" name="quiz" id="third" />
-            3. Co na mě obdivuješ?
-          </label>
-        </div>
-        <div className="quiz__btn">
-          <p className="quiz__text quiz__text--wrong">
-            Ou. Chyba. Zkusíš to znovu?
-          </p>
-          <button className="btn" type="reset">
-            Zkusit znovu
-          </button>
-          <button className="btn btn--secondary">Další lekce</button>
+          {answerResult === 'right' && (
+            <p className="quiz__text quiz__text--right">
+              Super! Máš to správně. Chceš jít dál?
+            </p>
+          )}
+          {answerResult === 'wrong' && (
+            <>
+              <p className="quiz__text quiz__text--wrong">
+                Ou. Chyba. Zkusíš to znovu?
+              </p>
+              <Btn text={'Zkus to znovu'} btnType={'secondary'} />
+            </>
+          )}
+          <Btn
+            text={answerResult === 'right' ? 'Odeslat' : 'Další lekce'}
+            btnType={answerResult === 'right' ? 'right' : 'link'}
+            linkTo={`/lekce/${aktualniLekce + 1}`}
+          />
         </div>
       </form>
     </>
