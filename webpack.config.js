9f7e7bde835439ce61426e3630ef7f8dffc0f834
hasilka <luciehasilova@gmail.com>
2021-05-30 14:27:53 +0200

    favicon

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         { from: 'src/assets', to: 'assets', noErrorOnMissing: true },
-        { from: 'src/favicon.ico', to: '', noErrorOnMissing: true },
+        { from: 'src/favicon.png', to: '', noErrorOnMissing: true },
         { from: 'src/index.html', to: '' },
         { from: 'src/public', to: 'public' },
         { from: 'src/_redirects', to: '' },
